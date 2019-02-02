d54aafff89a152988399e9a0e510576985f7a57e
GONBE <mrgonbegonbe@gmail.com>
2019-02-02 15:52:31 +0900

    upload

---
+++ upload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GONBE
+ * Date: 2019/02/02
+ * Time: 15:49
+ */
+
+if($_FILES['file']){
+  move_uploaded_file($_FILES['file']['tmp_name'], '/home/ec2-user/media/videos');
+}
+?>
+
+<form action="./normal_post1.php" method="POST" enctype="multipart/form-data">
+  <input type="file" name="file">
+  <input type="submit" value="ファイルをアップロードする">
+</form>
