0794db39af69b5482e33ec39c98b99fed75993e1
GONBE <mrgonbegonbe@gmail.com>
2019-03-09 05:38:26 +0900

    再生

--- upload.php
+++ upload.php
@@ -5,6 +5,15 @@
  * Date: 2019/02/02
  * Time: 15:49
  */
+// smarty のライブラリを読み込みます
+include_once __DIR__ . '/libs/Smarty.class.php';
+
+// smartyを宣言して設定を書き加えます
+$smarty = new Smarty();
+$smarty->escape_html = true;
+$smarty->template_dir = __DIR__ . '/templates';
+$smarty->compile_dir = __DIR__ . '/templates_c';
+
 
 $file = 'videos/' . basename($_FILES['file']['name']);
 move_uploaded_file($_FILES['file']['tmp_name'], $file);
