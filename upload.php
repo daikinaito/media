fe8743cba8cac68b1d784256425f2cc688e1def8
GONBE <mrgonbegonbe@gmail.com>
2019-03-09 05:32:36 +0900

    再生

--- upload.php
+++ upload.php
@@ -8,10 +8,3 @@
 
 $file = 'videos/' . basename($_FILES['file']['name']);
 move_uploaded_file($_FILES['file']['tmp_name'], $file);
-
-?>
-
-<form action="upload.php" method="POST" enctype="multipart/form-data">
-  <input type="file" name="file">
-  <input type="submit" value="ファイルをアップロードする">
-</form>
