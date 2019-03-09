f9e34429983dffbcca66856173ace2aa4b5c8869
GONBE <mrgonbegonbe@gmail.com>
2019-03-09 15:42:26 +0900

    database_confのdatabase名変更

--- database_conf.php
+++ database_conf.php
@@ -1,6 +1,6 @@
 <?php
 $DBSERVER = 'localhost';
-$DBNAME = 'board';
+$DBNAME = 'media';
 $DBUSER = 'gonbe'; //作成したユーザー名
 $DBPASSWD = 'apple'; //作成したユーザーのパスワード
 $dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8";
