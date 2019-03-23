a274d01b5b710d2c6c0cbddc1c8b82bd7b5b324e
GONBE <mrgonbegonbe@gmail.com>
2019-03-23 15:51:51 +0900

    insert

--- comment.php
+++ comment.php
@@ -6,21 +6,16 @@
  * Time: 15:15
  */
 
-session_start();
-
-if(isset($_SESSION['login'])==false){
-    header('Location: false.php');
-}
-
 $videoId = $_POST['videoId'];
 $currentTime = $_POST['currentTime'];
 $comment = $_POST['comment'];
+$userId = $_POST['userId'];
 
 require_once 'database_conf.php';
 $sql = 'INSERT INTO comments (comment, userId, videoId, currentTime, stamp) VALUES (:comment, :userId, :videoId, :currentTime, NOW())';
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':comment', $comment, \PDO::PARAM_STR);
-$stmt->bindValue(':userId', $_SESSION['id'], \PDO::PARAM_INT);
+$stmt->bindValue(':userId', $userId, \PDO::PARAM_INT);
 $stmt->bindValue(':videoId', $videoId, \PDO::PARAM_INT);
 $stmt->bindValue(':currentTime', $currentTime, \PDO::PARAM_INT);
 $stmt->execute();
